8841fff9ee55cc871c83a90c9f081d3eeafc6b9b
Kaili Nishihira and Lois Choi <lois@>
2017-07-11 15:52:49 -0700

    add jquery, add HTML forms, enter code for business/user logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var addDashes = function(inputWord) {
   var inputWordArray = inputWord.split('');
-  var vowels = ["a", "e", "i", "o", "u"];
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
   for (var i = 0; i < inputWordArray.length; i++) {
     var currentLetter = inputWordArray[i];
     if (vowels.indexOf(currentLetter) != -1) {
@@ -12,6 +12,14 @@ var addDashes = function(inputWord) {
 }
 
 $(document).ready(function() {
-  
+  $("#formOne").submit(function(event) {
+    var outputWord = addDashes($("input#word").val());
+    $("#results").text(outputWord);
+    $("#formOne").hide();
 
+    $("#showForm").click(function() {
+      $("#formOne").show();
+    })
+    event.preventDefault();
+  });
 });
